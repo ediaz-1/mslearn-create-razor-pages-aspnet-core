55ac25dc3608fe26ed12d875ab53c9aba594d8a4
ediaz-1 <ediaz@modern-woodmen.org>
2024-12-12 18:15:53 +0000

    finished webUI

--- ContosoPizza/Data/PizzaContext.cs
+++ ContosoPizza/Data/PizzaContext.cs
@@ -4,6 +4,8 @@ namespace ContosoPizza.Data
 {
     public class PizzaContext : DbContext
     {
+
+
         public PizzaContext(DbContextOptions<PizzaContext> options)
             : base(options)
         {
